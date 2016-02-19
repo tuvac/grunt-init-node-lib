13d47af253138b2a03659f65b227490f141a2e07
Stuart <stu.r.williams@gmail.com>
2016-02-19 23:48:20 +0000

    Initial Import

---
+++ root/README.md
@@ -0,0 +1,21 @@
+# {%= name %}
+
+{%= description %}
+
+## Getting Started
+Install the module with: `npm install {%= name %}`
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+_(Nothing yet)_
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}  
---
+++ template.js
@@ -0,0 +1,70 @@
+/*
+ * grunt-init-node
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a Node.js module, including Nodeunit unit tests.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
+    'be a unique ID not already in use at search.npmjs.org.';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'You should now install project dependencies with _npm ' +
+    'install_. After that, you may execute project tasks with _grunt_. For ' +
+    'more information about installing and configuring Grunt, please see ' +
+    'the Getting Started guide:' +
+    '\n\n' +
+    'http://gruntjs.com/getting-started';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+    init.process({
+        type: 'node'
+    }, [
+        // Prompt for these values.
+        init.prompt('name'),
+        init.prompt('title'),
+        init.prompt('description'),
+        init.prompt('version', '0.0.0'),
+        init.prompt('repository'),
+        init.prompt('author_name'),
+        init.prompt('author_email'),
+        init.prompt('node_version', '>= 0.10.0'),
+        init.prompt('main', 'index.js'),
+        init.prompt('npm_test', 'mocha'),
+    ], function(err, props) {
+        props.devDependencies = {
+            'grunt-contrib-jshint': '^0.12.0',
+            'grunt-git-release': '^0.1.3',
+            'grunt-mocha-test': '^0.12.7',
+            'mocha': '^2.4.5'
+        };
+
+        // Files to copy (and process).
+        var files = init.filesToCopy(props);
+
+        // Add properly-named license files.
+        //init.addLicenseFiles(files, props.licenses);
+
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props);
+
+        // Generate package.json file.
+        init.writePackageJSON('package.json', props);
+
+        // All done!
+        done();
+    });
+
+};
